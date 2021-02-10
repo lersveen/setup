cbea5abcca19e8ea0496ea024cc793d6a9953273
lersveen <lersveen@gmail.com>
2021-02-10 18:54:56 +0100

    automatic update: 10-02-2021 at 18:54:56

--- backup.sh
+++ backup.sh
@@ -1,9 +1,6 @@
 #!/bin/sh
 # Adapted from https://github.com/shubham1172/setup
-# This script helps me update my dot files regularly
-# It exists in my anacrontab as:
-#
-# @daily  10      setup.backup            su -c /home/shubham1172/setup/scripts/backup.sh - shubham1172 >> /home/shubham1172/.anacronlogs 2>&1
+# This script backs up config files regularly
 
 cd ${HOME}/setup
 
