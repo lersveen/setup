64c82fa0d12b962c0fdc605afedb0dfd1c6bab2a
lersveen <lersveen@gmail.com>
2021-02-11 16:10:25 +0100

    automatic update: 11-02-2021 at 16:10:25

--- backup.sh
+++ backup.sh
@@ -12,6 +12,14 @@ timestamp() {
   date +"%d-%m-%Y at %T"
 }
 
+git_repos() {
+	for d in $(find ~ -path ~/Library -prune -o -path ~/.gnupg -prune -o -path ~/.Trash -prune -o -name '.git' -print 2> /dev/null)
+	do 
+		cd $d/..
+		echo $(pwd | sed "s|${HOME}|~|") $(git config --get remote.origin.url)
+	done
+}
+
 # Use rsync to copy all files listed in backup file
 grep -v '^$\|^\s*\#' $BACKUP | awk NF | while read -r line; do
 	echo "$line" | awk '{ system("rsync " $1 " " $2) }'
@@ -27,11 +35,7 @@ brew list > ~/setup/brew/brewlist.txt
 code --list-extensions > ~/setup/vscode/vscode-extensions.txt
 
 # Export list of git repos
-for d in $(find ~ -path ~/Library -prune -o -path ~/.gnupg -prune -o -path ~/.Trash -prune -o -name '.git' -print 2> /dev/null)
-do 
-	cd $d/..
-	echo $(pwd | sed "s|${HOME}|~|") $(git config --get remote.origin.url)
-done > ~/setup/git/git-repos.txt
+
 
 # Get the stuff to Github
 if [[ $(git status --porcelain) ]]; then
