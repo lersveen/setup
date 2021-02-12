9ac7ace762db5de62105dd266951052f5ca364c0
lersveen <lersveen@gmail.com>
2021-02-12 16:45:45 +0100

    automatic update: 12-02-2021 at 16:45:44

--- test.sh
+++ test.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-out=$(exec ~/setup/backup.sh) 2>&1 | $out 1> >(tee >(logger)) || terminal-notifier -title "Config backup" -message "$out"
+out=$(exec ~/setup/backup.sh) 2>&1 || terminal-notifier -title "Config backup" -message "$out"
+1> >(tee >(logger))
