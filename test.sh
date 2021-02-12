4c1b70dffeb79683606edaed044d6113cd358121
lersveen <lersveen@gmail.com>
2021-02-12 16:02:41 +0100

    automatic update: 12-02-2021 at 16:02:41

--- test.sh
+++ test.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-exec 1> >(tee >(logger -t $(basename $0)) tee >(terminal-notifier -title "Config backup")) 2>&1
+exec 1> >(tee >(logger -t $(basename $0)) tee >(terminal-notifier -title "Config backup")) 2>&1 || echo 'errors???'
 exec ~/setup/backup.sh 2>&1
